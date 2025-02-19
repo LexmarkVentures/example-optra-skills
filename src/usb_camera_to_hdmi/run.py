bb4714f50178c442791d3326379b912bdfeb778c
Jack Warren <jack.warren@lexmark.com>
2025-02-19 11:27:35 -0500

    Added support for cx2k

--- src/usb_camera_to_hdmi/run.py
+++ src/usb_camera_to_hdmi/run.py
@@ -1,12 +1,29 @@
 import cv2
 import argparse
 
+def get_usb_camera_id():
+    for i in range(10):
+        cap = cv2.VideoCapture(i)
+        if cap.isOpened():
+            camera_id = i
+            cap.release()
+            return camera_id
+    return None
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--usb_camera_id', type=int, default=0)
+    parser.add_argument('--usb_camera_id', type=int, default=None)
     args = parser.parse_args()
 
-    cap = cv2.VideoCapture(args.usb_camera_id)
+    if args.usb_camera_id is None:
+        camera_id = get_usb_camera_id()
+        if camera_id is None:
+            print('USB Camera not found')
+            return
+    else:
+        camera_id = args.usb_camera_id
+
+    cap = cv2.VideoCapture(camera_id)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
